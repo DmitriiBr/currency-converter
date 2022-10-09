ef96b81c5ee032fb64ce1f82cea4d41d92760176
DBorisov <dm1.br@yandex.com>
2022-10-09 18:44:26 +0300

    merged

--- src/scripts/components/CurrencyInput/index.ts
+++ src/scripts/components/CurrencyInput/index.ts
@@ -7,17 +7,14 @@ export class MainCurrencyInput {
 
   render() {
     return `
-<<<<<<< HEAD
       <input type="text" class="${this.mainClass}"/>
-=======
-      <div class="${this.mainClass}__wrapper">
+        <div class="${this.mainClass}__wrapper">
         <input 
           type="number" 
           class="${this.mainClass}__field 
           ${this.mainClass}__field--currency"
         >
       </div>
->>>>>>> 3045061fa08f1043a4e3635a645e7d2b06ec3c4b
     `;
   }
 }
